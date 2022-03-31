b969bbd55f64a20efe523aa8b26a40dbff539fdd
majorchork <majorchork@gmail.com>
2022-03-31 01:34:31 +0100

    clean main function

--- main.go
+++ main.go
@@ -5,29 +5,15 @@ import (
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
 	"github.com/majorchork/blogapp/handlers"
-	"html/template"
 	"log"
 	"net/http"
-	"time"
 )
-
-var templates *template.Template
-
-type Blogpost struct {
-	PostTitle   string
-	PostData    string
-	WritersName string
-	Time        time.Time
-	Edit        string
-	Delete      string
-}
 
 func main() {
 	router := chi.NewRouter()
 	router.Use(middleware.Logger)
 	handlers.Run(router)
-	//http.Handle("/", router)
-	fmt.Println("Server started....")
+	fmt.Println("Server Running....")
 	err := http.ListenAndServe(":1769", router)
 	if err != nil {
 		log.Println(err)
